d48d48da7923575b669ae54f67fd88f3a23ddbd2
nipungupta09 <nipungupta.iitd@gmail.com>
2016-04-19 18:58:19 +0530

    Trying to correct bluetooth

--- app/src/main/java/com/nipungupta/helloworld/MainActivity.java
+++ app/src/main/java/com/nipungupta/helloworld/MainActivity.java
@@ -5,8 +5,10 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.speech.tts.TextToSpeech;
@@ -16,7 +18,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.DataInputStream;
 import java.io.DataOutput;
@@ -25,39 +31,81 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Locale;
+import java.util.Set;
 import java.util.UUID;
 
 public class MainActivity extends Activity {
 
+    private Button onBtn;
+    private Button offBtn;
+    private Button listBtn;
+    private Button findBtn;
+
     private static UUID MY_UUID = UUID.fromString("446118f0-8b1e-11e2-9e96-0800200c9a66");
+    private static final int REQUEST_ENABLE_BT = 1;
 
     private TextView tv;
+    private ListView listView;
+    private ArrayAdapter<String> BTArrayAdapter;
     private TextToSpeech textToSpeech;
     private String message;
 
     private BluetoothServerSocket mmServerSocket;
     private BluetoothAdapter mAdapter;
-    private BluetoothDevice remoteDevice;
+    private Set<BluetoothDevice> pairedDevices;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-      //  setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        tv = (TextView) findViewById(R.id.textView);
+        tv = (TextView) findViewById(R.id.text);
         mAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(mAdapter==null) {
+            onBtn.setEnabled(false);
+            offBtn.setEnabled(false);
+            listBtn.setEnabled(false);
+            findBtn.setEnabled(false);
+            tv.setText("Status: not supported");
+            Toast.makeText(getApplicationContext(),"Your device does not support Bluetooth",Toast.LENGTH_LONG).show();
+        } else {
+            onBtn = (Button) findViewById(R.id.turnOn);
+            onBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    on(v);
+                }
+            });
+
+            offBtn = (Button) findViewById(R.id.turnOff);
+            offBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    off(v);
+                }
+            });
+
+            listBtn = (Button) findViewById(R.id.paired);
+            listBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    list(v);
+                }
+            });
+
+            findBtn = (Button) findViewById(R.id.search);
+            findBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    find(v);
+                }
+            });
+
+            listView = (ListView) findViewById(R.id.listView1);
+            BTArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        }
+
         textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
@@ -67,7 +115,73 @@ public class MainActivity extends Activity {
             }
         });
 
-        turnOnBluetooth();
+   //     turnOnBluetooth();
+    }
+
+    public void on(View view) {
+        if(!mAdapter.isEnabled()) {
+            Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(intent, REQUEST_ENABLE_BT);
+            Toast.makeText(getApplicationContext(), "Bluetooth turned on", Toast.LENGTH_LONG).show();
+        }
+        else {
+            Toast.makeText(getApplicationContext(), "Bluetooth is already on", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode==REQUEST_ENABLE_BT) {
+            if(mAdapter.isEnabled()) {
+                tv.setText("Status: Enabled");
+            } else {
+                tv.setText("Status: Disabled");
+            }
+        }
+    }
+
+    public void list(View view) {
+        pairedDevices = mAdapter.getBondedDevices();
+        tv.setText("Status: "+pairedDevices.size()+" paired devices found");
+        for(BluetoothDevice device : pairedDevices)
+            BTArrayAdapter.add(device.getName()+"\n"+device.getAddress());
+
+        Toast.makeText(getApplicationContext(), "Show Paired Devices", Toast.LENGTH_LONG).show();
+    }
+
+    final BroadcastReceiver bReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if(BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                BTArrayAdapter.add(device.getName()+"\n"+device.getAddress());
+                BTArrayAdapter.notifyDataSetChanged();
+            }
+        }
+    };
+
+    public void find(View view) {
+        if(mAdapter.isDiscovering()) {
+            mAdapter.cancelDiscovery();
+        }
+        else {
+            BTArrayAdapter.clear();
+            mAdapter.startDiscovery();
+            registerReceiver(bReceiver, new IntentFilter(BluetoothDevice.ACTION_FOUND));
+        }
+    }
+
+    public void off(View view) {
+        mAdapter.disable();
+        tv.setText("Status: Disconnected");
+        Toast.makeText(getApplicationContext(), "Bluetooth turned off", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(bReceiver);
     }
 
     protected void turnOnBluetooth() {
